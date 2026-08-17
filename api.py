1b51394d640100e88c3623b98c8c8e62e7e12bca
Astr0n0 <rezababaei3120@gmail.com>
2026-08-17 15:45:29 +0330

    commiting the last changes on api.py

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
@@ -15,6 +16,18 @@ app = FastAPI(
     version="0.1.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost",
+        "http://127.0.0.1",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # =========================
 # Schemas
